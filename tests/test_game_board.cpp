93b0c55064d30cc99e07d88de0ae60bd53fbcc27
svalenciasan <52926410+svalenciasan@users.noreply.github.com>
2020-12-04 01:41:46 -0600

    Added player testing.

--- tests/test_game_board.cpp
+++ tests/test_game_board.cpp
@@ -4,10 +4,17 @@
 using deviousdungeon::gameboard::GameBoard;
 
 TEST_CASE("Board Generation", "[procedural][board]") {
-SECTION("Constructor") {
-  GameBoard game_board(5, 5);
-  size_t a = 5;
-  size_t b = 9;
-  auto g = a - b;
+  SECTION("Constructor") {
+    GameBoard game_board(5, 5);
+    assert(game_board.GetGameState() == deviousdungeon::gameboard::kInProgress);
+    assert(game_board.GetLevel() == 1);
+    assert(game_board.GetScore() == 0);
+    assert(game_board.GetRows() == 5);
+    assert(game_board.GetColumns() == 5);
+    assert(game_board.GetPlayer().GetHealth() == 3);
+  }
 }
+TEST_CASE("Movement", "move") {
+  GameBoard game_board(5, 5);
+
 }
--- tests/test_player.cpp
+++ tests/test_player.cpp
@@ -1,10 +1,81 @@
 #include <catch2/catch.hpp>
 #include <core/player.h>
+#include <core/weapon.h>
 
 using deviousdungeon::player::Player;
+using deviousdungeon::weapon::Weapon;
+using deviousdungeon::weapon::WeaponType;
 
-TEST_CASE("Update particle", "[update][particle]") {
-SECTION("Update <0,0> velocity") {
-
+TEST_CASE("Heal Player", "[heal]") {
+  Player player;
+  Enemy enemy(2);
+SECTION("Healing to full health") {
+  assert(player.GetMaxHealth() == 3);
+  assert(player.GetHealth() == 3);
+  player.UseMelee(enemy);
+  assert(player.GetMaxHealth() == 3);
+  assert(player.GetHealth() == 1);
+  player.Heal(2);
+  assert(player.GetMaxHealth() == 3);
+  assert(player.GetHealth() == 3);
 }
+SECTION("Healing past full health") {
+    assert(player.GetMaxHealth() == 3);
+    assert(player.GetHealth() == 3);
+    player.UseMelee(enemy);
+    assert(player.GetMaxHealth() == 3);
+    assert(player.GetHealth() == 1);
+    player.Heal(5);
+    assert(player.GetMaxHealth() == 3);
+    assert(player.GetHealth() == 3);
+}
+}
+TEST_CASE("Pickup Weapon", "[weapon]") {
+  Player player;
+  SECTION("No Weapons") {
+    assert(player.GetWeapon(deviousdungeon::weapon::kMeleeWeapon).GetPower() == 0);
+    assert(player.GetWeapon(deviousdungeon::weapon::kRangedWeapon).GetPower() == 0);
+  }
+  SECTION("Picking up weapons with no weapons in inventory") {
+    Weapon melee_weapon(deviousdungeon::weapon::kMeleeWeapon,5);
+    Weapon ranged_weapon(deviousdungeon::weapon::kRangedWeapon,5);
+
+    player.EquipWeapon(melee_weapon);
+    player.EquipWeapon(ranged_weapon);
+
+    assert(player.GetWeapon(deviousdungeon::weapon::kMeleeWeapon).GetPower() == 5);
+    assert(player.GetWeapon(deviousdungeon::weapon::kRangedWeapon).GetPower() == 5);
+  }
+  SECTION("Picking up weapons with weaker weapons in inventory") {
+    Weapon melee_weapon(deviousdungeon::weapon::kMeleeWeapon,5);
+    Weapon ranged_weapon(deviousdungeon::weapon::kRangedWeapon,5);
+
+    player.EquipWeapon(melee_weapon);
+    player.EquipWeapon(ranged_weapon);
+
+    Weapon melee_weapon2(deviousdungeon::weapon::kMeleeWeapon,6);
+    Weapon ranged_weapon2(deviousdungeon::weapon::kRangedWeapon,6);
+
+    player.EquipWeapon(melee_weapon2);
+    player.EquipWeapon(ranged_weapon2);
+
+    assert(player.GetWeapon(deviousdungeon::weapon::kMeleeWeapon).GetPower() == 6);
+    assert(player.GetWeapon(deviousdungeon::weapon::kRangedWeapon).GetPower() == 6);
+  }
+  SECTION("Picking up weapons with stronger weapons in inventory") {
+    Weapon melee_weapon(deviousdungeon::weapon::kMeleeWeapon,5);
+    Weapon ranged_weapon(deviousdungeon::weapon::kRangedWeapon,5);
+
+    player.EquipWeapon(melee_weapon);
+    player.EquipWeapon(ranged_weapon);
+
+    Weapon melee_weapon2(deviousdungeon::weapon::kMeleeWeapon,2);
+    Weapon ranged_weapon2(deviousdungeon::weapon::kRangedWeapon,2);
+
+    player.EquipWeapon(melee_weapon2);
+    player.EquipWeapon(ranged_weapon2);
+
+    assert(player.GetWeapon(deviousdungeon::weapon::kMeleeWeapon).GetPower() == 2);
+    assert(player.GetWeapon(deviousdungeon::weapon::kRangedWeapon).GetPower() == 2);
+  }
 }
